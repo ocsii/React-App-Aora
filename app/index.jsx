97bdceedde086fd062ec510eefb4e7ab998bc893
Christian Cham <christianfintacham@gmail.com>
2024-08-24 14:10:27 +0800

    Add custom button and update index

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,55 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View, Image } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { Redirect, router } from "expo-router";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack"> Aora! </Text>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
 
-      <StatusBar style="auto" />
+          <Image
+            source={images.cards}
+            className="max-w--[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
 
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to profile
-      </Link>
-    </View>
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-b old text-center">
+              Discover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Aora </Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+            />
+          </View>
+
+          <Text className="text-sm text-gray-100 mt-7 text-center font-pregular">
+            {" "}
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora{" "}
+          </Text>
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
---
+++ components/FormVield.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FormVield = () => {
+  return (
+    <View>
+      <Text>FormVield</Text>
+    </View>
+  );
+};
+
+export default FormVield;
